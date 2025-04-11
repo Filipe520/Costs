e592fff837f11a35ba3799b7b9ff352e29b24cee
Filipe520 <filipealves520@gmail.com>
2025-04-11 19:49:07 -0300

    Exibindo detalhes do projeto

--- src/components/pages/Project/Project.jsx
+++ src/components/pages/Project/Project.jsx
@@ -1,29 +1,70 @@
 import styles from "./Project.module.css";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
+import Loading from "../../layout/Loading/Loading";
+import Container from "../../layout/Container/Container";
 
 const Project = () => {
   const { id } = useParams();
   const [project, setProject] = useState({});
+  const [showProjectForm, setShowProjectForm] = useState(false);
 
   useEffect(() => {
-    fetch(`http://localhost:5000/projects/${id}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setProject(data);
+    setTimeout(() => {
+      fetch(`http://localhost:5000/projects/${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
       })
-      .catch((error) => console.error("Error fetching project:", error));
+        .then((response) => response.json())
+        .then((data) => {
+          setProject(data);
+        })
+        .catch((err) => console.log(err));
+    }, 1000);
   }, [id]);
+
+  function toggleProjectForm() {
+    setShowProjectForm(!showProjectForm);
+  }
+
   return (
-    <div className={styles.project_container}>
-      <h1>{project.name}</h1>
-      <p>This is the project page.</p>
-    </div>
+    <>
+      {project.name ? (
+        <div className={styles.project_details}>
+          <Container customClass="column">
+            <div className={styles.details_container}>
+              <h1>Projeto: {project.name}</h1>
+              <button className={styles.btn} onClick={toggleProjectForm}>
+                {showProjectForm ? "Fechar projeto" : "Editar projeto"}
+              </button>
+              {!showProjectForm ? (
+                <div className={styles.project_info}>
+                  <p>
+                    <span>Categoria:</span> {project.category.name}
+                  </p>
+                  <p>
+                    <span>Total de Orçamentos:</span> {project.budget}
+                  </p>
+                  <p>
+                    <span>Total Utilizado:</span> {project.cost}
+                  </p>
+                </div>
+              ) : (
+                <div className={styles.project_info}>
+                  <p>detalhes do projeto</p>
+                </div>
+              )}
+            </div>
+          </Container>
+        </div>
+      ) : (
+        <div className={styles.project_container}>
+          <Loading />
+        </div>
+      )}
+    </>
   );
 };
 
